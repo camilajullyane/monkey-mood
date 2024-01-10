6fed484ca136e28dec917abca30155649cf2d97c
camilajullyane <camilaj.silva0@gmail.com>
2024-01-10 17:53:37 -0300

    Co-authored-by: Victor Salgado <devictor8@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,30 @@
+const macaco = {
+    macaco1: 'Macaco Chato',
+    macaco2: 'Macaco Despirocado',
+    macaco3: 'Macaco Toquinho',
+    macaco4: 'Macaco Fofo',
+    macaco5: 'Macaco Safado',
+    macaco6: 'Macaco Linguarudo',
+    macaco7: 'Macaco Homofóbico',
+    macaco8: 'Macaco Praieiro',
+    macaco9: 'Macaco Modo Sério',
+    macaco10: 'Macaco amimir',
+    macaco11: 'Macaco Julgador',
+    macaco12: 'Macaco Sofredor',
+    macaco13: 'Macaco Sensual',
+    macaco14: 'Macaco Sorridente',
+    macaco15: 'Macaco Depressivo',
+    macaco16: 'Macaco Preguiça',
+    macaco17: 'Macaco Trouxa',
+    macaco18: 'Macaco Rabugento',
+    macaco19: 'Macaco Fofoqueiro',
+    macaco20: 'Macaco Desoline'
+}
+const decricao = 
+const imagem = documento.getElementById("img");
+
+const sortearMacaco = () => {
+    let num = Math.floor(Math.random() * 20)
+
+
+}
